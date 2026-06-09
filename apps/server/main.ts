6d914fb6c378726851b2e56528d0ebf87cace258
Shobhnik13 <shobhnikw@gmail.com>
2026-06-10 01:06:19 +0530

    added db connection

--- apps/server/main.ts
+++ apps/server/main.ts
@@ -2,24 +2,31 @@ import express, { type Request, type Response } from "express";
 import { Logger } from "./common/logger";
 import { errorHandler } from "./common/error-handler";
 import { authRouter } from "./routes/auth-router";
+import { connectDb } from "./db";
 
 const app = express();
 const log = new Logger({ stdout: true });
 
 app.use(express.json());
 
-// health check endpoint
 app.get("/health", (_: Request, res: Response) => {
   res.status(200).send("API SERVER HEALTHY");
 });
 
-// route declarations
 app.use("/auth", authRouter);
 
-// global error handler
 app.use(errorHandler);
 
-// start the server
-app.listen(8080, () => {
-  log.info("server running on http://localhost:8080");
+async function start() {
+  await connectDb();
+  log.info("connected to postgres database");
+
+  app.listen(8080, () => {
+    log.info("server running on http://localhost:8080");
+  });
+}
+
+start().catch((err) => {
+  log.error(`failed to start: ${err.message}`);
+  process.exit(1);
 });
